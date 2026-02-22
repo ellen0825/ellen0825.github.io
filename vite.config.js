2a2b7532e5f856fa6f41970fc679d4334ead43cc
ellen0825 <ellenmartinelli0825@outlook.com>
2026-02-22 07:40:22 -0800

    2026-2-22-4

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
-})
+// export default defineConfig({
+//   plugins: [
+//     react({
+//       babel: {
+//         plugins: [['babel-plugin-react-compiler']],
+//       },
+//     }),
+//   ],
+// })
+export default {
+  base: '/ellen0825.github.io/', // GitHub Pages에 배포할 때의 경로
+};
